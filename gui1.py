e4067373aa634ff9e68ef90d6db59db2d3053228
jsyblue <129835643+jsyblue@users.noreply.github.com>
2026-04-21 01:16:29 +0100

    Create gui1.py

---
+++ gui1.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+#add a timer system later
+
+
+win = tk.Tk()
+win.title("gui")
+#button click event
+
+title = tk.Label(win,text='encrypted transfer system')
+title.grid(column=0,row=0)
+
+#text input box &send button
+text = tk.StringVar() #textbox
+textbox = tk.Entry(win, width=25,textvariable=text)
+textbox.grid(column=0,row=1)
+textbox.focus()
+
+
+def send_button():#send button
+    send.configure(text=" x ")
+
+
+send = tk.Button(win,text=" send > ",command=send_button)
+send.grid(column=1,row=1)
+
+win.mainloop()
